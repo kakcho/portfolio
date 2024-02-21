88848cacb451a91f484dedb231dbe6ca228e491a
kakcho <nagaev_maks@inbox.ru>
2024-02-21 16:49:16 +0500

    portfolio

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -11,8 +11,8 @@ const footer = () => {
     <div className="container">
       <div className="footer__wrapper">
         <ul className="social">
-          <li className="social__item"><a href="#!"><img src={vk} alt="Link" /></a></li>
-          <li className="social__item"><a href="#!"><img src={gitHub} alt="Link" /></a></li>
+          <li className="social__item"><a href="https://vk.com/maksimwigga"><img src={vk} alt="Link" /></a></li>
+          <li className="social__item"><a href="https://github.com/kakcho/"><img src={gitHub} alt="Link" /></a></li>
         </ul>
         <div className="copyright">
         </div>
